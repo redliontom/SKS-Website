622cc761c8e5f411c0c9582a69edc4f8bffa5631
redliontom <tomschalbar@hotmail.com>
2014-12-16 15:17:32 +0100

    Warehouse

--- public/js/controllers/package.js
+++ public/js/controllers/package.js
@@ -5,8 +5,8 @@ angular.module('sksApp').controller('PackageCtrl', function($scope, $http){
     $scope.sendPackage = function () {
         if ($scope.frmPackage.$valid) {
 
-            var data = {Firstname : $scope.Firstname, Lastname: $scope.Lastname, Street: $scope.Street, PostalCode: $scope.PostalCode,
-                City: $scope.City, Weight: $scope.Weight};
+            var data = {Firstname : $scope.Firstname, Lastname : $scope.Lastname, Street : $scope.Street, PostalCode : $scope.PostalCode,
+                City : $scope.City, Weight : $scope.Weight};
             $http.post('/Package', data)
                 .success(function (data) {
                     console.log('success');
