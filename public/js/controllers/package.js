125a8f904248162a6376e2d3993fe9790c4df838
redliontom <tomschalbar@hotmail.com>
2014-12-16 14:56:20 +0100

    AngularJS Zeug

--- public/js/controllers/package.js
+++ public/js/controllers/package.js
@@ -5,9 +5,13 @@ angular.module('sksApp').controller('PackageCtrl', function($scope, $http){
     $scope.sendPackage = function () {
         if ($scope.frmPackage.$valid) {
 
+<<<<<<< HEAD
             var data = {FirstName : $scope.firstname, LastName : $scope.lastname, Street : $scope.street, PostalCode : $scope.plz,
                 City : $scope.city, Country: $scope.country, Weight : $scope.weight};
             console.log(data);
+=======
+            var data = new FormData(this);
+>>>>>>> 7ebaac8... AngularJS Zeug
             $http.post('/Package', data)
                 .success(function (data) {
                     console.log('success');
--- public/js/controllers/warehouse.js
+++ public/js/controllers/warehouse.js
@@ -2,7 +2,10 @@
 
 angular.module('sksApp').controller('WarehouseCtrl', function($scope){
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> 7ebaac8... AngularJS Zeug
 });
 
